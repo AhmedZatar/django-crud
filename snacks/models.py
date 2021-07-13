10da240438881c55ff691c36dfb9f268139f9f2f
ahmedzatar <ahmed.zatar2@gmail.com>
2021-07-13 16:28:08 +0300

    finish lab28

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+class Snack(models.Model):
+    name= models.CharField(max_length=64)
+    purchaser= models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description=models.TextField()
+    
+    def __str__ (self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('snack_detail', args=[self.id])
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Snack
 
-# Create your tests here.
+class SnakesTests(TestCase):
+    def setUp(self):
+        self.user=get_user_model().objects.create_user(
+            username='admin',
+            email='admin@gmail.com',
+            password='admin123'
+
+        )
+        self.snake=Snack.objects.create(
+            name='Hummus',
+            purchaser=self.user,
+            description='afasgishjgoi hgoiseghiogsioogsegoi',
+
+        )
+    
+    def test_list_page(self):
+        url= reverse('snacks_list')
+        self.assertTemplateUsed(self.client.get(url), 'snack_list.html')
+        self.assertEqual(self.client.get(url).status_code,200)
+    
+    def test_detail_page(self):
+        response= self.client.get(
+            reverse('snack_detail',args='1') ,
+            {'name':'Hummus', 'purchaser':self.user , 'description':'afasgishjgoi hgoiseghiogsioogsegoi'},
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_page(self):
+        response= self.client.get(
+            reverse('snack_detail',args='1') ,
+            {'name':'Hummus', 'purchaser':self.user , 'description':'afasgishjgoi hgoiseghiogsioogsegoi'},
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_page(self):
+        response= self.client.get(
+            reverse('snack_create') ,
+            {'name':'Hummus', 'purchaser':self.user , 'description':'afasgishjgoi hgoiseghiogsioogsegoi'},
+        )
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_page(self):
+        response= self.client.get(
+            reverse('snack_update',args='1') ,
+            {'name':'Hummus', 'purchaser':self.user , 'description':'afasgishjgoi hgoiseghiogsioogsegoi'},
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_page(self):
+        response= self.client.get(
+            reverse('snack_delete',args='1') ,
+            {'name':'Hummus', 'purchaser':self.user , 'description':'afasgishjgoi hgoiseghiogsioogsegoi'},
+        )
+        self.assertEqual(response.status_code, 200)
